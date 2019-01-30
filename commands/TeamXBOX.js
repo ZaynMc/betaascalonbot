0120c128cd4144dbe96a4e2cb6578cb6c1d09217
ZaynMc <theobourrier@outlook.fr>
2019-01-30 13:46:17 +0100

    Update Role

--- commands/TeamXBOX.js
+++ commands/TeamXBOX.js
@@ -5,9 +5,21 @@ module.exports.run = async (bot, message, args) => {
   let role = message.guild.roles.find(r => r.name === "XBOX");
   if(message.member.roles.has(roleID)) {
     console.log(`Yay, the author of the message has the role!`);
+
+      var embed = new Discord.RichEmbed()
+    .setDescription(`${message.member.user} a rejoint la team` + role);
+
+    message.channel.send(embed);
+
     message.member.removeRole(role).catch(console.error);
   } else {
     console.log(`Nope, noppers, nadda.`);
+
+    var embed = new Discord.RichEmbed()
+    .setDescription(`${message.member.user} a quitter la team` + role);
+
+    message.channel.send(embed);
+
     message.member.addRole(role).catch(console.error);
   }
 }
