aed87686906399b67982542cc5827b01a73356ba
ZaynMc <theobourrier@outlook.fr>
2019-01-30 12:31:36 +0100

    Update PS4.js

--- commands/PS4.js
+++ commands/PS4.js
@@ -2,12 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, ops, tools) => {
 
+  console.log("CLAP");
+
   let roleID = "540122138422804481";
   let role = message.guild.roles.find(r => r.name === "PS4");
 
   if(message.member.roles.has(roleID.id)) {
   message.member.removeRole(role).catch(console.error);
-} else {
+  return;
+}
   message.member.addRole(role).catch(console.error);
 }
 }
