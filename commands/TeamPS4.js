1a90de50c9f78826990fdef8d149b6bfdfc7e792
ZaynMc <theobourrier@outlook.fr>
2019-01-30 13:34:01 +0100

    Update TeamPS4.js

--- commands/TeamPS4.js
+++ commands/TeamPS4.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let roleID = "540121221971443714";
+  let role = message.guild.roles.find(r => r.name === "PS4");
   if(message.member.roles.has(roleID)) {
     console.log(`Yay, the author of the message has the role!`);
     message.member.removeRole(role).catch(console.error);
