4373d624d86ff20a65acecf45d9f025820a011a0
ZaynMc <theobourrier@outlook.fr>
2019-01-31 16:38:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,15 +18,14 @@ fs.readdir("./commands/", (err, files) => {
     let props = require(`./commands/${f}`);
     console.log(`${i + 1}: ${f} loaded!`);
     bot.commands.set(props.help.name, props);
-    props.help.aliases.forEach(alias => {
-        bot.aliases.set(alias, props.help.name);
-    });
   });
 });
 
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+
+  bot.user.setStatus('dnd'); // dnd, idle, online, invisible
 });
 
 bot.on("message", async message => {
