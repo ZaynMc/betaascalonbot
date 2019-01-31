9f4a9efdb4d3c58e50a5a3474634d7958b7e1f42
ZaynMc <theobourrier@outlook.fr>
2019-01-31 20:16:43 +0100

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -6,10 +6,36 @@ module.exports.run = async (bot, message, args) => {
       message.delete();
       return;
   }
+  if(args[0] == null) {
+    message.delete();
+      return;
+  }
+  if(args[0] = "solo") {
+
   modRole = message.guild.roles.find('name', 'Snipe Annonce');
   const messageSnipe = message.member.guild.channels.find('name', '📣snipe-annonces📣');
 
   messageSnipe.send(`📣 ANNONCE SNIPE 📣 [${modRole}]\nMode : **SOLO**\nPlatforme : **TOUS [Pc, Xbox, PS4]**\nRejoignez le channel : 📞 VOCAL SNIPE 📞\n \n Commence dans 5 minutes...`);
+  return;
+}
+
+if(args[0] = "duo") {
+
+modRole = message.guild.roles.find('name', 'Snipe Annonce');
+const messageSnipe = message.member.guild.channels.find('name', '📣snipe-annonces📣');
+
+messageSnipe.send(`📣 ANNONCE SNIPE 📣 [${modRole}]\nMode : **DUO**\nPlatforme : **TOUS [Pc, Xbox, PS4]**\nRejoignez le channel : 📞 VOCAL SNIPE 📞\n \n Commence dans 5 minutes...`);
+return;
+}
+
+if(args[0] = "squad") {
+
+modRole = message.guild.roles.find('name', 'Snipe Annonce');
+const messageSnipe = message.member.guild.channels.find('name', '📣snipe-annonces📣');
+
+messageSnipe.send(`📣 ANNONCE SNIPE 📣 [${modRole}]\nMode : **SQUAD**\nPlatforme : **TOUS [Pc, Xbox, PS4]**\nRejoignez le channel : 📞 VOCAL SNIPE 📞\n \n Commence dans 5 minutes...`);
+return;
+}
 }
 
 module.exports.help = {
