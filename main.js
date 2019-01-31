d50bbe353a4a782c9b9e5e1b2ccad68eb0aee847
ZaynMc <theobourrier@outlook.fr>
2019-01-31 15:14:24 +0100

    update

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
+
 const token = process.env.token;
 
 fs.readdir("./commands/", (err, files) => {
@@ -19,7 +21,7 @@ fs.readdir("./commands/", (err, files) => {
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
     bot.commands.set(props.help.name, props);
-    props.config.alias.forEach(alias => {
+    props.config.aliases.forEach(alias => {
       bot.aliases.set(alias, props.help.name)
     });
   });
