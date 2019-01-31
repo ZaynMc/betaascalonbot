07f34dd3a7229725c70ee52ee40aa0cbbcb79eaa
ZaynMc <theobourrier@outlook.fr>
2019-01-31 16:31:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,30 +3,25 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
 const token = process.env.token;
 
+// Load commands
+bot.commands = new discord.Collection();
 fs.readdir("./commands/", (err, files) => {
+  if (err) console.error(err);
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
 
-  if(err) console.log(err);
+  if (jsfiles.length <= 0) return console.log("There are no commands to load...");
 
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
+  console.log(`Loading ${jsfiles.length} commands...`);
+  jsfiles.forEach((f, i) => {
     let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
+    console.log(`${i + 1}: ${f} loaded!`);
     bot.commands.set(props.help.name, props);
     props.help.aliases.forEach(alias => {
-      bot.aliases.set(alias, props.help.name);
+        bot.aliases.set(alias, props.help.name);
     });
-
   });
-
 });
 
 
