f2fec7d1005b5e2d5e8451ef3b9ce59d863798fe
ZaynMc <theobourrier@outlook.fr>
2019-01-31 20:31:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,40 @@ const bot = new Discord.Client({disableEveryone: true});
 
 const token = process.env.token;
 
+const size = 12;
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = '#'+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {
+    bot.guilds.get("539552660806696988").roles.find('name', "Raimbow").setColor(rainbow[place])
+		.catch(console.error);
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
+    }
+  }
+}
+
 // Load commands
 bot.commands = new Discord.Collection();
 fs.readdir("./commands/", (err, files) => {
@@ -23,6 +57,9 @@ fs.readdir("./commands/", (err, files) => {
 
 
 bot.on("ready", async () => {
+
+  setInterval(changeColor, 10);
+
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 
 
