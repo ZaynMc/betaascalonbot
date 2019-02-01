439827873cc704357fae08fac27f64eeafa9438e
ZaynMc <theobourrier@outlook.fr>
2019-02-01 15:24:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -96,10 +96,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
         console.log('xbox');
         let roleID = "540121484690194432";
         let role = reaction.message.guild.roles.find(r => r.name === "XBOX");
-        if(reaction.message.member.roles.has(roleID)) {
-          console.log(`Yay, the author of the message has the role!`);
-          reaction.message.member.removeRole(role).catch(console.error);
-        } else {
+        if(!reaction.message.member.roles.has(roleID)) {{
           console.log(`Nope, noppers, nadda.`);
           reaction.message.member.addRole(role).catch(console.error);
         }
@@ -108,10 +105,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
       if(reaction.emoji.name == "ps4") {
         let roleID = "540121221971443714";
         let role = reaction.message.guild.roles.find(r => r.name === "PS4");
-        if(reaction.message.member.roles.has(roleID)) {
-          console.log(`Yay, the author of the message has the role!`);
-          reaction.message.member.removeRole(role).catch(console.error);
-        } else {
+        if(!reaction.message.member.roles.has(roleID)) {
           console.log(`Nope, noppers, nadda.`);
           reaction.message.member.addRole(role).catch(console.error);
         }
@@ -120,10 +114,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
       if(reaction.emoji.name == "switch") {
         let roleID = "540121715506806785";
         let role = reaction.message.guild.roles.find(r => r.name === "SWITCH");
-        if(reaction.message.member.roles.has(roleID)) {
-          console.log(`Yay, the author of the message has the role!`);
-          reaction.message.member.removeRole(role).catch(console.error);
-        } else {
+        if(!reaction.message.member.roles.has(roleID)) {
           console.log(`Nope, noppers, nadda.`);
           reaction.message.member.addRole(role).catch(console.error);
         }
@@ -132,10 +123,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
       if(reaction.emoji.name == "pc") {
         let roleID = "540121554374361099";
         let role = reaction.message.guild.roles.find(r => r.name === "PC");
-        if(reaction.message.member.roles.has(roleID)) {
-          console.log(`Yay, the author of the message has the role!`);
-          reaction.message.member.removeRole(role).catch(console.error);
-        } else {
+        if(!reaction.message.member.roles.has(roleID)) {
           console.log(`Nope, noppers, nadda.`);
           reaction.message.member.addRole(role).catch(console.error);
         }
@@ -144,10 +132,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
       if(reaction.emoji.name == "mobile") {
         let roleID = "540121685064417280";
         let role = reaction.message.guild.roles.find(r => r.name === "Mobile");
-        if(reaction.message.member.roles.has(roleID)) {
-          console.log(`Yay, the author of the message has the role!`);
-          reaction.message.member.removeRole(role).catch(console.error);
-        } else {
+        if(!reaction.message.member.roles.has(roleID)){
           console.log(`Nope, noppers, nadda.`);
           reaction.message.member.addRole(role).catch(console.error);
         }
@@ -156,7 +141,57 @@ bot.on('messageReactionAdd', (reaction, user) => {
 });
 
 bot.on('messageReactionRemove', (reaction, user) => {
+  if(reaction.message.channel.name == "📕rôles"){
+    console.log('a reaction has been added');
 
+    if(reaction.emoji.name == "xbox"){
+      console.log('xbox');
+      let roleID = "540121484690194432";
+      let role = reaction.message.guild.roles.find(r => r.name === "XBOX");
+      if(reaction.message.member.roles.has(roleID)) {
+        console.log(`Yay, the author of the message has the role!`);
+        reaction.message.member.removeRole(role).catch(console.error);
+      }
+    }
+
+    if(reaction.emoji.name == "ps4") {
+      let roleID = "540121221971443714";
+      let role = reaction.message.guild.roles.find(r => r.name === "PS4");
+      if(reaction.message.member.roles.has(roleID)) {
+        console.log(`Nope, noppers, nadda.`);
+        reaction.message.member.removeRole(role).catch(console.error);
+      }
+    }
+
+    if(reaction.emoji.name == "switch") {
+      let roleID = "540121715506806785";
+      let role = reaction.message.guild.roles.find(r => r.name === "SWITCH");
+      if(reaction.message.member.roles.has(roleID)) {
+        console.log(`Nope, noppers, nadda.`);
+        reaction.message.member.removeRole(role).catch(console.error);
+      }
+    }
+
+    if(reaction.emoji.name == "pc") {
+      let roleID = "540121554374361099";
+      let role = reaction.message.guild.roles.find(r => r.name === "PC");
+      if(reaction.message.member.roles.has(roleID)) {
+        console.log(`Nope, noppers, nadda.`);
+        reaction.message.member.removeRole(role).catch(console.error);
+      }
+    }
+
+    if(reaction.emoji.name == "mobile") {
+      let roleID = "540121685064417280";
+      let role = reaction.message.guild.roles.find(r => r.name === "Mobile");
+      if(reaction.message.member.roles.has(roleID)){
+        console.log(`Nope, noppers, nadda.`);
+        reaction.message.member.removeRole(role).catch(console.error);
+      }
+    }
+
+
+  }
 });
 
   bot.login(token);
