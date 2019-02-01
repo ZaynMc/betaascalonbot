57c4261117b1b56b615221b77e8cf61c6d232623
ZaynMc <theobourrier@outlook.fr>
2019-02-01 19:16:18 +0100

    Update Roles.js

--- commands/Roles.js
+++ commands/Roles.js
@@ -6,12 +6,16 @@ exports.run = async (client, message, args) => {
      const c = message.guild.roles.get('540121715506806785');
      const d  = message.guild.roles.get('540121554374361099');
      const e = message.guild.roles.get('540121685064417280');
+     const f = message.guild.roles.get('540278913230700584');
+     const g = message.guild.roles.get('540477037094240257');
 
      const xbox = client.emojis.find(emoji => emoji.name === "xbox");
      const ps4 = client.emojis.find(emoji => emoji.name === "ps4");
      const sw = client.emojis.find(emoji => emoji.name === "switch");
      const pc = client.emojis.find(emoji => emoji.name === "pc");
      const mobile = client.emojis.find(emoji => emoji.name === "mobile");
+     const battleroyale = client.emojis.find(emoji => emoji.name === "Battle Royale");
+     const creatif = client.emojis.find(emoji => emoji.name === "Mode Créatif");
 
      const embed = new Discord.RichEmbed()
      .setTitle('Rôles Disponibles')
@@ -21,6 +25,9 @@ exports.run = async (client, message, args) => {
        ${sw} ${c}
        ${pc} ${d}
        ${mobile} ${e}
+       ${battleroyale} ${f}
+       ${creatig} ${g}
+
      `
      )
      .setColor(0xdd9323)
@@ -31,6 +38,8 @@ exports.run = async (client, message, args) => {
        msg.react(sw.id);
        msg.react(pc.id);
        msg.react(mobile.id);
+       msg.react("🏹");
+       msg.react("🔨");
      });
 
      }
