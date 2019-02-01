e9a14bf622b72d7b8e9299d94080cd2f20f3bd48
ZaynMc <theobourrier@outlook.fr>
2019-02-01 15:19:04 +0100

    Update Roles.js

--- commands/Roles.js
+++ commands/Roles.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args) => {
      message.delete ().catch(O_o => {});
      const a = message.guild.roles.get('540121484690194432');
      const b = message.guild.roles.get('540121221971443714');
-     const c = message.guild.roles.get('540582229169012746');
+     const c = message.guild.roles.get('540819216786259988');
      const d  = message.guild.roles.get('540121554374361099');
      const e = message.guild.roles.get('540121685064417280');
 
