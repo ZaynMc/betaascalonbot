22c077fdbfc59e3e5af33c02d2e9db4649a982a8
ZaynMc <theobourrier@outlook.fr>
2019-02-01 15:08:02 +0100

    Update Roles.js

--- commands/Roles.js
+++ commands/Roles.js
@@ -28,7 +28,7 @@ exports.run = async (client, message, args) => {
      .setColor(0xdd9323)
 
      message.channel.send(embed).then(msg => {
-       msg.react(xbox + "");
+       msg.react(':xbox:');
 
      });
 
