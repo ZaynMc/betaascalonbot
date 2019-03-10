24cd89a96fc7a46eade7c7af04bd1c1369607c0a
ZaynMc <theobourrier@outlook.fr>
2019-03-10 19:40:32 +0100

    Update FortniteCommand.js

--- commands/FortniteCommand.js
+++ commands/FortniteCommand.js
@@ -11,9 +11,40 @@ module.exports = class TestCommand {
 
     run(bot, message, args) {
 
-        var headers = {
+        fortnite(args[1], args[2]).then((data) => {
+            var STAT = new Discord.RichEmbed()
+            .setTitle("__***Fortnite Stats***__")
+            .setURL(data.info.url)
+            .addField("------------------------------------",
+                               "Account Username: " + "__**" + data.info.username + "**__" + "\n" +
+                               "Account Platform: " + "__**" + data.info.platform + "**__" + "\n" +
+                               "------------------------------------\n" +
+                               "Total Kills: " + "__**" + data.lifetimeStats[10].value + "**__" + "\n" +
+                               "Total Wins: " + "__**" + data.lifetimeStats[7].value + "**__" + "\n" +
+                               "Total Top 3: " + "__**" + data.lifetimeStats[2].value + "**__" + "\n" +
+                               "Total Top 5: " + "__**" + data.lifetimeStats[1].value + "**__" + "\n" +
+                               "Total Top 6: " + "__**" + data.lifetimeStats[3].value + "**__" + "\n" +
+                               "Total Top 10: " + "__**" + data.lifetimeStats[0].value + "**__" + "\n" +
+                               "Total Top 12: " + "__**" + data.lifetimeStats[4].value + "**__" + "\n" +
+                               "Total Top 25: " + "__**" + data.lifetimeStats[5].value + "**__" + "\n" +
+                               "Total Matches Played: " + "__**" + data.lifetimeStats[6].value + "**__" + "\n" +
+                               "------------------------------------\n", true)
+    
+            .setColor("0x#FF0000")
+    
+            message.channel.send(STAT);
+    
+          }).catch(function(err) {
+            message.channel.send(err);
+          })
+    }   
+
+
+    /**  var headers = {
             'TRN-Api-Key': 'ef632672-b4a0-4708-90f0-a7f03176512a'
           }
+
+          fortnite()
 
           let arg = message.content.split(" ").slice(1);
             let botmessage = arg.join(" ");
@@ -64,6 +95,4 @@ module.exports = class TestCommand {
                                  "------------SQUAD STATS-----------" + "\n" +
                                  Squad + "\n"+ "```");
         })
-    }
-
-    }   
+    }*/
