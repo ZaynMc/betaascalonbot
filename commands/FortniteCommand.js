881ff971c84cecee05b03825090258342a56698c
ZaynMc <theobourrier@outlook.fr>
2019-03-10 19:41:49 +0100

    Update FortniteCommand.js

--- commands/FortniteCommand.js
+++ commands/FortniteCommand.js
@@ -36,63 +36,6 @@ module.exports = class TestCommand {
     
           }).catch(function(err) {
             message.channel.send(err);
-          })
+          });
+        }
     }   
-
-
-    /**  var headers = {
-            'TRN-Api-Key': 'ef632672-b4a0-4708-90f0-a7f03176512a'
-          }
-
-          fortnite()
-
-          let arg = message.content.split(" ").slice(1);
-            let botmessage = arg.join(" ");
-        
-          var options = {
-            url: `https://api.fortnitetracker.com/v1/profile/` + `pc` + "/" + `${botmessage}`,
-            method: 'GET',
-            headers: headers
-          }
-        
-          request(options, function (error, response, body) {
-            var info = JSON.parse(body);
-        
-            var LifeTime = "";
-            var Solo = "";
-            var Duo = "";
-            var Squad = "";
-        
-            for(var currentStatIndex = 0; currentStatIndex < info.lifeTimeStats.length; currentStatIndex++) {
-                LifeTime += info.lifeTimeStats[currentStatIndex].key + ": " + info.lifeTimeStats[currentStatIndex].value + "\n";
-            }
-        
-            for(var STATS in info.stats.p2){
-                Solo += info.stats.p2[STATS].label + ": " + info.stats.p2[STATS].displayValue + "\n";
-            }
-        
-            for(var STATS_1 in info.stats.p10){
-                Duo += info.stats.p10[STATS_1].label + ": " + info.stats.p10[STATS_1].displayValue + "\n";
-            }
-        
-            for(var STATS_2 in info.stats.p9){
-                Squad += info.stats.p9[STATS_2].label + ": " + info.stats.p9[STATS_2].displayValue + "\n";
-            }
-        
-            message.channel.send("```" + "\n" +
-                                 "----------------------------------" + "\n" + 
-                                 "Account Username: " + info.epicUserHandle + "\n" +
-                                 "Account Platform: " + info.platformNameLong + "\n" +
-                                 "----------LIFETIME STATS----------" + "\n" + 
-                                 LifeTime + "\n" +
-        
-                                 "------------SOLO STATS------------" + "\n" +
-                                 Solo + "\n" +
-        
-                                 "------------DUO STATS-------------" + "\n" +
-                                 Duo + "\n" +
-        
-                                 "------------SQUAD STATS-----------" + "\n" +
-                                 Squad + "\n"+ "```");
-        })
-    }*/
