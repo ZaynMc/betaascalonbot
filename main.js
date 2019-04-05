db5fde7e9faf2f97755736e1e4f6359d0800589d
ZaynMc <theobourrier@outlook.fr>
2019-04-05 19:39:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -28,6 +28,34 @@ client.on("message", (message) => {
   }
 });
 
+client.on('raw', event => {
+  if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+     
+      let channel = client.channels.get(event.d.channel_id);
+      let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+      let user = msg.guild.members.get(event.d.user_id);
+     
+      if (msg.author.id == client.user.id && msg.content != initialMessage){
+     
+          var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+          var role = msg.content.match(re)[1];
+     
+          if (user.id != client.user.id){
+              var roleObj = msg.guild.roles.find(r => r.name === role);
+              var memberObj = msg.guild.members.get(user.id);
+             
+              if (event.t === "MESSAGE_REACTION_ADD"){
+                  memberObj.addRole(roleObj);
+              } else {
+                  memberObj.removeRole(roleObj);
+              }
+          }
+      }
+      }),
+    };
+  });
+
+
 const token = process.env.token;
 client.login(token);
 
