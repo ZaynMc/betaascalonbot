0e37b0a61a5bcbe474b18f0e6745286dbb45590a
ZaynMc <theobourrier@outlook.fr>
2019-04-24 19:57:08 +0200

    Update Message.js

--- events/Message.js
+++ events/Message.js
@@ -9,15 +9,12 @@ module.exports = (client) => {
     client.on('messageReactionAdd', (reaction, user) => {
         let channel = client.channels.get(reaction.message.channel.id);
         let message = channel.fetchMessage(reaction.message.id).then(msg => {
-            let user = msg.guild.members.get(user.id);
     
             if (msg.author.id == client.user.id && msg.content != message){
                 console.log("BONJOUR");
             }
     
         });
-    
-    
     });
     
 }
