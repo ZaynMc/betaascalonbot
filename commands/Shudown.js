39b50805921ffd69b86975e51ef1a5bcbb7f4389
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-26 17:26:48 +0200

    Update Shudown.js

--- commands/Shudown.js
+++ commands/Shudown.js
@@ -5,33 +5,33 @@ module.exports = class HelpCommand {
         this.usage = ""
     }
 
-    run(bot, message, args) {
+run(bot, message, args) {
 	    
-	    console.log("test1);
+	console.log("test1);
 
-        if(message.author.id != 195159794636685313 || message.author.id != 184730747079229441) return;
-		
-		
+        if(message.author.id == 195159794636685313 || message.author.id == 184730747079229441) {
+
 		console.log("test2);
-        const embed = new Discord.RichEmbed()
-        .setTitle("Shutdown Bot")
-        .setAuthor("Thibaut Black", "https://media.discordapp.net/attachments/566329831667793920/569607425364066314/unknown.png")
+		const embed = new Discord.RichEmbed()
+		.setTitle("Shutdown Bot")
+		.setAuthor("Thibaut Black", "https://media.discordapp.net/attachments/566329831667793920/569607425364066314/unknown.png")
 
-        .setColor(0x00AE86)
-        .setFooter("Créer par Zayn", "https://media.discordapp.net/attachments/566329831667793920/569607425364066314/unknown.png")
-        .setThumbnail("https://i.imgur.com/3ubvmFE.png")
+		.setColor(0x00AE86)
+		.setFooter("Créer par Zayn", "https://media.discordapp.net/attachments/566329831667793920/569607425364066314/unknown.png")
+		.setThumbnail("https://i.imgur.com/3ubvmFE.png")
 		.setDescription('Shutdown du bot en cours')
-		
+
 		.addField("Shudown Initiateur",message.author.username, true)
- 
-        message.channel.send({embed});
-        
+
+		message.channel.send({embed});
+
 		var Zayn = bot.fetchUser(184730747079229441);
 		var Thibaut = bot.fetchUser(195159794636685313);
-		
+
 		Zayn.send({embed});
 		Thibaut.send({embed});
-		
+
 		process.exit();
+	    }
     }
 }
