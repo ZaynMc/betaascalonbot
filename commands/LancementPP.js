4028dc8d26269f9686130a984330536f7f033212
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-26 21:44:39 +0200

    Update LancementPP.js

--- commands/LancementPP.js
+++ commands/LancementPP.js
@@ -8,19 +8,20 @@ module.exports = class ClearCommand {
     run(bot, message, args) {
 
         let isAdmin = message.member.hasPermission("MANAGE_CHANNELS");
-        if(!isAdmin) return;
+        //if(!isAdmin) return;
 
         var args1 = args[1];
         var args2 = args[2];
 
         const config = require("../config.json");
         const annoncepp = message.member.guild.channels.find('id', config.channel.ppannonce);
-        let role = message.guild.roles.find(r => r.name === config.role.notifpp);
+        let role = message.guild.roles.get(config.role.notifpp);
 
-        annoncepp.send(`:loudspeaker: [${role}] Lancement Game Partie Personnalisée !\n\n:red_circle: Mode de Jeux : ${args1}\n:key: Code Game : ${args2}\n\n:point_right: Lancement de la Game 1 minute après ce message !`);
+
+        annoncepp.send(`(Message de Test)📢 [${role}] Lancement Game Partie Personnalisée !\n\n:red_circle: Mode de Jeux : ${args1}\n:key: Code Game : ${args2}\n\n:point_right: Lancement de la Game 1 minute après ce message !`);
 
         timer:setTimeout(function(){
-            annoncepp.send(`:loudspeaker: [${role}] Lancement de la Game !\n\nBonne chance à tou(te)s`);
+            annoncepp.send(`(Message de Test)📢 [${role}] Lancement de la Game !\n\nBonne chance à tou(te)s`);
             annoncepp.send('', {
                 files: [
                     "../bot/images/ligne.png"
