8d860f3f7466c819ea13c208929a4c4e41210a5c
ZaynMc <theobourrier@outlook.fr>
2019-04-26 17:24:36 +0200

    Update Shudown.js

--- commands/Shudown.js
+++ commands/Shudown.js
@@ -6,11 +6,13 @@ module.exports = class HelpCommand {
     }
 
     run(bot, message, args) {
+	    
+	    console.log("test1);
 
         if(message.author.id != 195159794636685313 || message.author.id != 184730747079229441) return;
 		
 		
-		
+		console.log("test2);
         const embed = new Discord.RichEmbed()
         .setTitle("Shutdown Bot")
         .setAuthor("Thibaut Black", "https://media.discordapp.net/attachments/566329831667793920/569607425364066314/unknown.png")
