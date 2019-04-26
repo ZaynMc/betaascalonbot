4922c36e6e8fe3c5985afe024a695284e9977d0f
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-26 22:31:49 +0200

    Update EmojiReaction.js

--- events/EmojiReaction.js
+++ events/EmojiReaction.js
@@ -7,8 +7,7 @@ module.exports = (client) => {
     let initialMessage = `**:bell: __Réction Notification Partie Perso__ :bell:**`;
 
     client.on('raw', event => {
-        console.log(event.t);
-        if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        if (event.t == 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
            
             let channel = client.channels.get(event.d.channel_id);
             let message = channel.fetchMessage(event.d.message_id).then(msg=> {
