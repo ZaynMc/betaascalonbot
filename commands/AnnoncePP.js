7d6e6d542811ebaaffcaf9ff84477736757e20d7
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-26 21:36:00 +0200

    Update AnnoncePP.js

--- commands/AnnoncePP.js
+++ commands/AnnoncePP.js
@@ -8,12 +8,12 @@ module.exports = class ClearCommand {
     run(bot, message, args) {
 
         let isAdmin = message.member.hasPermission("MANAGE_CHANNELS");
-        //if(!isAdmin) return;
+        if(!isAdmin) return;
 
         const config = require("../config.json");     
         const annoncepp = message.member.guild.channels.find('id', config.channel.ppannonce);
         let role = message.guild.roles.get(config.role.notifpp);
 
-        annoncepp.send(`(Message de Test)📢 [${role}] Nous allons bientôt lancer des parties personnalisés ! Rejoignez le channel vocal "${config.channel.tournoigeneralvoc}"`);
+        annoncepp.send(`📢 [${role}] Nous allons bientôt lancer des parties personnalisés ! Rejoignez le channel vocal "${config.channel.tournoigeneralvoc}"`);
         }
     }   
