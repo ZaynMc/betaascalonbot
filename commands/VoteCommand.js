9d869a972222e61b702aecbc19393b87eaed2845
ZaynMc <theobourrier@outlook.fr>
2019-04-26 20:50:35 +0200

    Create VoteCommand.js

---
+++ commands/VoteCommand.js
@@ -0,0 +1,11 @@
+module.exports = class ClearCommand {
+    constructor() {
+        this.name = "annoncepp",
+        this.alias = ['anpp', 'ppannonce'],
+        this.usage = "a!annoncepp"
+    }
+
+    run(bot, message, args) {
+
+        }
+    }   
