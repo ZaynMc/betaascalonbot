0c32753fcee63ed27ba607195a51a4ed03a29747
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-27 19:51:11 +0200

    Update VoteCommand.js

--- commands/VoteCommand.js
+++ commands/VoteCommand.js
@@ -18,17 +18,23 @@ module.exports = class ClearCommand {
                 var react1 = message.reactions.find('🙍').count;
                 var react2 = message.reactions.find('👭').count;
                 var react3 = message.reactions.find('👥').count;
+                console.log("1")
                 if (react1 > react2) {
+                    console.log("2")
                     if(react1 > react3) {
+                        console.log("3")
                         message.channel.send(`Le mode de jeux qui a gagné est __**Duo**__ : 🙍`);   
                     } else {
+                        console.log("4")
                         message.channel.send(`Le mode de jeux qui a gagné est __**Squad**__ : 👥`);
                     }
-                }
-                if (react2 > react1) {
+                } else {
+                    console.log("5")
                     if(react2 > react3) {
+                        console.log("6")
                         message.channel.send(`Le mode de jeux qui a gagné est __**Duo**__ : 👭`);    
                     } else {
+                        console.log("7")
                         message.channel.send(`Le mode de jeux qui a gagné est __**Squad**__ : 👥`);
                     }
                 }
