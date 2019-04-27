14cf459950ecd52f1615ea43ebffde82749d114b
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-27 14:11:40 +0200

    Update MessageRoleCreate.js

--- commands/MessageRoleCreate.js
+++ commands/MessageRoleCreate.js
@@ -10,14 +10,14 @@ module.exports = class ClearCommand {
         message.delete();
 
         message.channel.send("Roles Menu : Notif \n Mettez une réaction à ce message pour avoir le rôle lié à la réaction \n \n <:LiguePlatine:481501980435415040> : __**Notif Boutique**__ \n \n ▶ : __**Notif News**__ \n\n <:LigueChallenger:481501768480718849> : __**Notif Sondage**__ \n \n <:notif_discord:516187882709581825> : __**Notif Pub**__ \n \n 🔑 : __**Notif PP**__ ").then(msg => {
-            let emoji1 = msg.guild.emojis.find('name',"notif_discord");
-			let emoji2 = msg.guild.emojis.find('name',"LigueChallenger");
-			let emoji3 = msg.guild.emojis.find('name',"LiguePlatine");
+            let emoji1 = msg.guild.emojis.find('name', "notif_discord");
+	    let emoji2 = msg.guild.emojis.find('name', "LigueChallenger");
+            let emoji3 = msg.guild.emojis.find('name', "LiguePlatine");
             msg.react("🔑");
             msg.react("▶");
-			msg.react("emoji1");
-			msg.react("emoji2");
-			msg.react("emoji3");
+	    msg.react("emoji1");
+	    msg.react("emoji2");
+	    msg.react("emoji3");
         });
         }
     } 
