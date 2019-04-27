1e831280f3b0797c6c0df77e75d6531e976f50f4
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-27 13:56:36 +0200

    Update MessageRoleCreate.js

--- commands/MessageRoleCreate.js
+++ commands/MessageRoleCreate.js
@@ -9,9 +9,9 @@ module.exports = class ClearCommand {
     run(bot, message, args) {
         message.delete();
 
-        message.channel.send("Roles Menu : Notif \n Mettez une réaction à ce message pour avoir le rôle lié à la réaction \n \n Notif Boutique : <:LiguePlatine:481501980435415040>").then(msg => {
+        message.channel.send("Roles Menu : Notif \n Mettez une réaction à ce message pour avoir le rôle lié à la réaction \n \n <:LiguePlatine:481501980435415040> : __**Notif Boutique**__ \n \n ▶ : __**Notif News**__ <:LigueChallenger:481501768480718849> : __**Notif Sondage**__ \n \n <:notif_discord:516187882709581825> : __**Notif Pub**__ \n \n 🔑 : __**Notif PP**__ ").then(msg => {
             msg.react("<:LiguePlatine:481501980435415040>");
-            msg.react("❌");
+            msg.react("▶");
         });
         }
     } 
