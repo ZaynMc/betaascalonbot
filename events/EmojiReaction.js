309a5613d83e5329e19b5c2729333ca2a2779a26
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-27 14:52:53 +0200

    Update EmojiReaction.js

--- events/EmojiReaction.js
+++ events/EmojiReaction.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
 
     client.on('raw', event => {
         if (event.t == 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
-    //        console.log(event.t)
+            console.log(event.d.emoji.name)
             console.log(event.d)
             
             let channel = client.channels.get(event.d.channel_id);
