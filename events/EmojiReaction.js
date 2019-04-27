b3dc8b6a61fe2f90cba7d69b0d9c63c07d859c66
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-27 15:29:49 +0200

    Update EmojiReaction.js

--- events/EmojiReaction.js
+++ events/EmojiReaction.js
@@ -17,24 +17,45 @@ module.exports = (client) => {
                     var memberObj = msg.guild.members.get(user.id);    
                     if(event.d.emoji.name == "LiguePlatine") {
                        console.log("Emojy detected Ligue Platine")
+                        var roleObj = msg.guild.roles.get(config.role.notifboutique2);
+                        if (event.t === "MESSAGE_REACTION_ADD"){
+                            memberObj.addRole(roleObj);
+                        } else {
+                            memberObj.removeRole(roleObj);
+                        }
                     } else if (event.d.emoji.name == "LigueChallenger") {
                        console.log("Emojy detected Ligue Challenger")
+                        var roleObj = msg.guild.roles.get(config.role.notifsondage);
+                        if (event.t === "MESSAGE_REACTION_ADD"){
+                            memberObj.addRole(roleObj);
+                        } else {
+                            memberObj.removeRole(roleObj);
+                        }
                     } else if (event.d.emoji.name == "▶") {
                        console.log("Emojy detected Arrow")
+                        var roleObj = msg.guild.roles.get(config.role.notifnews);
+                        if (event.t === "MESSAGE_REACTION_ADD"){
+                            memberObj.addRole(roleObj);
+                        } else {
+                            memberObj.removeRole(roleObj);
+                        }
                     } else if (event.d.emoji.name == "notif_discord") {
                        console.log("Emojy detected  Notif Discord")
+                        var roleObj = msg.guild.roles.get(config.role.notifpub);
+                        if (event.t === "MESSAGE_REACTION_ADD"){
+                            memberObj.addRole(roleObj);
+                        } else {
+                            memberObj.removeRole(roleObj);
+                        } 
                     } else if (event.d.emoji.name == "🔑") {
                        console.log("Emojy detected Clée")
+                        var roleObj = msg.guild.roles.get(config.role.notifpp);
+                        if (event.t === "MESSAGE_REACTION_ADD"){
+                            memberObj.addRole(roleObj);
+                        } else {
+                            memberObj.removeRole(roleObj);
+                        }  
                     }
- 
-                    
-                    var roleObj = msg.guild.roles.get(config.role.notifpp);
-                    if (event.t === "MESSAGE_REACTION_ADD"){
-                        memberObj.addRole(roleObj);
-                    } else {
-                        memberObj.removeRole(roleObj);
-                    }
-
                 //}
             }
             
