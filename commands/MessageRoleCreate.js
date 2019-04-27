65808f30db729efb3e86872a7be36cfdc759fc60
ZaynMc <theobourrier@outlook.fr>
2019-04-27 20:12:07 +0200

    Update MessageRoleCreate.js

--- commands/MessageRoleCreate.js
+++ commands/MessageRoleCreate.js
@@ -9,10 +9,7 @@ module.exports = class ClearCommand {
     run(bot, message, args) {
         message.delete();
 
-	let isAdmin = message.member.hasPermission("MANAGE_CHANNELS");
-        if(!isAdmin) return;
-	    
-        message.channel.send("Roles Menu : Notif \n Mettez une réaction à ce message pour avoir le rôle lié à la réaction \n \n <:LiguePlatine:481501980435415040> : __**Notif Boutique**__ \n \n ▶ : __**Notif News**__ \n\n <:LigueChallenger:481501768480718849> : __**Notif Sondage**__ \n \n <:notif_discord:516187882709581825> : __**Notif Pub**__ \n \n 🔑 : __**Notif PP**__ ").then(msg => {
+        message.channel.send("**Roles Menu : Notif** \n Mettez une réaction à ce message pour avoir le rôle lié à la réaction \n \n <:LiguePlatine:481501980435415040> : __**Notif Boutique**__ \n \n ▶ : __**Notif News**__ \n\n <:LigueChallenger:481501768480718849> : __**Notif Sondage**__ \n \n <:notif_discord:516187882709581825> : __**Notif Pub**__ \n \n 🔑 : __**Notif PP**__ ").then(msg => {
             let emoji1 = msg.guild.emojis.find('id', "notif_discord");
 	    let emoji2 = msg.guild.emojis.find('name', "LigueChallenger");
             let emoji3 = msg.guild.emojis.find('name', "LiguePlatine");
