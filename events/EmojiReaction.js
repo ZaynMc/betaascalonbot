f9349d98bb0634402e16f9b963fe8c82cfc00776
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-27 12:59:18 +0200

    Update EmojiReaction.js

--- events/EmojiReaction.js
+++ events/EmojiReaction.js
@@ -9,12 +9,10 @@ module.exports = (client) => {
     client.on('raw', event => {
         if (event.t == 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
             
-            let user = msg.guild.members.get(event.d.user_id);
-            if (user.id == client.user.id) return;
-            
             let channel = client.channels.get(event.d.channel_id);
             let message = channel.fetchMessage(event.d.message_id).then(msg=> {
-            
+            let user = msg.guild.members.get(event.d.user_id);
+            if (user.id == client.user.id) return;
 
             if (msg.author.id == client.user.id){
 
