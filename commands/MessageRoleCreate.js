eb1ff8a8fd061f8d3126c6b87361f756f8152f9a
trmaud <30575266+trmaud@users.noreply.github.com>
2019-04-27 13:13:00 +0200

    Update MessageRoleCreate.js

--- commands/MessageRoleCreate.js
+++ commands/MessageRoleCreate.js
@@ -5,15 +5,13 @@ module.exports = class ClearCommand {
         this.usage = "a!RoleCR"
     }
 
+
     run(bot, message, args) {
+        message.delete();
 
-        let isAdmin = message.member.hasPermission("MANAGE_CHANNELS");
-        if(!isAdmin) return;
-
-        const config = require("../config.json");     
-        const annoncepp = message.member.guild.channels.find('id', config.channel.ppannonce);
-        let role = message.guild.roles.get(config.role.notifpp);
-
-        annoncepp.send(`📢 [${role}] Nous allons bientôt lancer des parties personnalisés ! Rejoignez le channel vocal "${config.channel.tournoigeneralvoc}"`);
+        message.channel.send("Choissez votre mode de jeux : \n \n __**Arène**__ : ✅ \n __**Normal**__ : ❌").then(msg => {
+            msg.react("✅");
+            msg.react("❌");
+        });
         }
-    }   
+    } 
