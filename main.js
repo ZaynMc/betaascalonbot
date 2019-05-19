6ffea8c2bf05b12109eb8c4357c5516f2086187e
trmaud <30575266+trmaud@users.noreply.github.com>
2019-05-19 09:27:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -334,5 +334,37 @@ client.on("message", async message => {
     }
   };
 
+   bot.on("message", async message => {
+      if(message.channel.type === "dm") return;
+    
+      // Part 1 : checking & removing the text
+      //1 blacklisted words
+    let blacklisted = ['discord.gg'] //words put , after the word
+    
+    //2 looking for words
+    let foundInText = false;
+    for (var i in blacklisted) { // loops through the blacklisted list
+      if(message.channel.name == "👍partenariat" ||message.channel.name == "🤝échange-de-pub🤝" || message.channel.name == "👆petite-pub" || message.member.hasPermission("MANAGE_MESSAGES")) return;
+      
+    
+      if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
+    }
+    // checks casesensitive words
+    
+    //3 deletes and send message
+      if (foundInText) {
+        //message delete
+        message.delete();
+        //message send to author
+        message.author.send('La pub de discord sur le serveur ASCALON est interdite !');
+        //message channel send to author for advertissment
+        message.channel.send(`<@${message.author.id}> Bonjour, la pub de serveur discord est interdite. Dernière avertissement avant le ban`).then(message => message.delete(5000));
+    
+    
+      }
+    
+    });
+
+
   //client.login("NTcxMjg1OTc4MzY3MzkzNzky.XMgv8A.4hJ4Kki2-BudbU_dqM6EZWig59o");
 client.login(process.env.TOKEN);
